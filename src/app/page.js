16b7ed8dab14befc7de02fc7e68228453fe58bce
znath <contactznath@gmail.com>
2025-09-24 20:47:49 +0530

    adds the useEffect hook for popular movies display

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,50 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MovieCard from "@/components/MovieCard";
+import { GetPopularMovies, SearchMovies } from "./lib/api";
 
 const Home = ()=> {
   const[query, setQuery] = useState("");
+  const[movies, setMovies] = useState([]);
+  const[loading, setLoading] = useState(false);
+  const[error, setError]= useState(null);
 
-  const handleSearch= (e)=>{
-   console.log("searched and submitted")
+  useEffect(()=>{
+    const fetchPopular = async()=>{
+      try{
+        setLoading(true);
+        const popular = await GetPopularMovies();
+        setMovies(popular);
+      }
+      catch(err){
+        setError("Failed to fetch movies, try again.")
+      }
+      finally{
+        setLoading(false);
+      }
+    }
+    fetchPopular()
+  },[]);
+
+  const handleSearch= async (e)=>{
+    e.preventDefault();
+   console.log("searched and submitted");
+
+   try{
+    setLoading(true);
+    const results= await SearchMovies(query);
+    setQuery(results);
+   }
+   catch(err){
+    setError("failed to search for requested movie, try a different movie instead.")
+   }
+   finally{
+    setLoading(false)
+   }
   }
+
+
 
   return(
     <main className="p-6">
