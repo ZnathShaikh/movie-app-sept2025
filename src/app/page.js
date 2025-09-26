b8930649f03ae8027c5293a47ab28d3d8159a93e
znath <contactznath@gmail.com>
2025-09-26 20:11:51 +0530

    adds the favorites

--- src/app/page.js
+++ src/app/page.js
@@ -9,6 +9,28 @@ const Home = ()=> {
   const[movies, setMovies] = useState([]);
   const[loading, setLoading] = useState(false);
   const[error, setError]= useState(null);
+  const [favorites, setFavorites]= useState([]);
+
+  useEffect(()=>{
+    const saved= localStorage.getItem("favorites");
+    if(saved){
+      setFavorites(JSON.parse(saved));
+    }
+  }, [])
+
+  useEffect(()=>{
+    localStorage.setItem("favorites", JSON.stringify(favorites))
+  }, [favorites]);
+
+  const handleFavorite= (movie)=> {
+    setFavorites((prev)=>{
+      if (prev.some((fav)=> fav.id === movie.id)){
+        return prev.filter((fav)=> fav.id !== movie.id);
+      } else{
+        return[...prev, movie];
+      }
+    } );
+  };
 
   useEffect(()=>{
     const fetchPopular = async()=>{
